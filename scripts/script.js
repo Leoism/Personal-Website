949908ddf2b6669f5513838a39476836d55a7b89
Leoism <leoism17@gmail.com>
2018-07-28 21:32:18 -0700

    Add a point feature this is still not done there are still many things that need to be done hopefully by the end of Tuesday a working build can be published

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,8 @@ function loadGame() {
 		window.location.href = 'game/game_start.html'
 	}, 3000)
 }
+
+function addPoints() {
+	let points = document.getElementById("points")
+	points.innerHTML += 1
+}
