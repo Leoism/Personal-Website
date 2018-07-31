557ae5faa2d5178a82c78d79cf31ef37d4ddf344
Leoism <leoism17@gmail.com>
2018-07-31 10:46:04 -0700

    Add faster movement, and warping

--- game/sketch.js
+++ game/sketch.js
@@ -19,8 +19,15 @@ function draw() {
 
 function step() {
   control()
+  collisions()
   applyGravity()
   keyIsDown()
+}
+
+function collisions() {
+  if(myBox.y >= myBox2.y + 6&& myBox.y <= myBox2.y) {
+    myBox.x = 0
+  }
 }
 
 function applyGravity() {
@@ -29,14 +36,26 @@ function applyGravity() {
   } else {
     myBox.y = height/9
   }
+
+  if (myBox.x <= 0) {
+    myBox.x = width
+  } else if (myBox.x >= width) {
+    myBox.x = 0
+  }
 }
 
 function control() {
-    if (keyIsDown(RIGHT_ARROW)) {
+    if (keyIsDown(LEFT_ARROW) && keyIsDown(DOWN_ARROW)) {
+      myBox.x -= HorizMv;
+      myBox.y += 3
+    } else if (keyIsDown(RIGHT_ARROW) && keyIsDown(DOWN_ARROW)) {
+      myBox.x += HorizMv;
+      myBox.y += 3
+    } else if (keyIsDown(DOWN_ARROW)) {
+      myBox.y += 3
+    } else if (keyIsDown(RIGHT_ARROW)) {
       myBox.x += HorizMv
     } else if (keyIsDown(LEFT_ARROW)) {
       myBox.x -= HorizMv
-    } else {
-
     }
 }
